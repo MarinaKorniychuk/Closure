942ebe0c6abd0d50b08a7408fda05ea5aeedc894
Marina korniychuk <kornij4uk@gmail.com>
2017-02-21 01:29:02 +0300

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function sum() {
-  // your solution
+module.exports = function sum(...a) {
+  if (a.length == 0) return 0;
+  return function(...b) {
+    if (b.length == 0) { 
+      a.reduce = [].reduce;
+      return a.reduce((a, b) => a + b, 0);
+    } 
+    a.concat = [].concat;
+    return sum.apply(this, (a.concat(b)));
+  }
 }
+
+
+/*module.exports = function sum() {
+  this.storage = this.storage || 0;
+
+  if (arguments.length > 0) {
+    arguments.reduce = [].reduce;
+    this.storage += arguments.reduce((a, b) => a + b, 0);
+    return sum;
+  }
+
+  let res = this.storage;
+  this.storage = 0;
+  return res;
+}*/
